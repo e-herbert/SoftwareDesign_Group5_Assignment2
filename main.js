e5346c6f1967161c6b10ba0c65b3d8724c63df85
jingo0 <94132372+jingo0@users.noreply.github.com>
2022-02-18 18:01:29 -0600

    edited

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ $(function () {
         }
     });
 });
+
+(function($){"use strict";})(jQuery);
+
+async function getQuote()
+{
+    // var quote_data = document.querySelector('#quote');
+    // quote_data.innerHTML="<b>Suggested Price: $###<br/>  Total Amount Due: $###<br/>"
+
+    alert("Suggested Price: $3.55\nTotal Amount Due: $350.00")
+        
+    
+    //quote_data.innerHTML += "<b>Ticket Number: " + booking[i].ticket_no + "  |  </b>Booking Reference: " + booking[i].book_ref + "  |  Passenger ID: " + booking[i].passenger_id + "<br/>";
+    
+}
