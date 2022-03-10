47758f389c02790c766b7aabe343bb86c1ed0529
jingo0 <94132372+jingo0@users.noreply.github.com>
2022-03-09 23:57:55 -0600

    Create table.sql

---
+++ table.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS userdata CASCADE;
+DROP TABLE IF EXISTS userProfile CASCADE;
+DROP TABLE IF EXISTS history CASCADE;
+
+CREATE TABLE userdata (
+    username VARCHAR(32) NOT NULL,
+    pass VARCHAR(32) NOT NULL,
+    PRIMARY KEY(username)
+);
+
+CREATE TABLE userProfile (
+    username VARCHAR(32) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
+    address1 VARCHAR(100) NOT NULL,
+    address2 VARCHAR(50) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    zipcode INTEGER NOT NULL,
+    CONSTRAINT "userProfile_username_fkey" FOREIGN KEY (username) REFERENCES userdata(username)
+);
+
+CREATE TABLE history (
+    username VARCHAR(32) NOT NULL,
+    date timestamp NOT NULL,
+    gallons numeric(4,2) NOT NULL,
+    suggested_price numeric(6,2) NOT NULL,
+    CONSTRAINT "history_username_fkey" FOREIGN KEY (username) REFERENCES userdata(username)
+);
