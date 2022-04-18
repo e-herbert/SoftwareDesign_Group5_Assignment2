872e8cf3dae0c7bdb4df8b451b8e3be06b727118
jingo0 <94132372+jingo0@users.noreply.github.com>
2022-04-17 20:09:14 -0500

    Update table.sql

--- table.sql
+++ table.sql
@@ -25,25 +25,25 @@ CREATE TABLE history (
     gallons numeric(10,2) NOT NULL,
     suggested_price numeric(10,2) NOT NULL,
     total_price numeric(10,2) NOT NULL,
-    address VARCHAR(200) NOT NULL
+    address VARCHAR(200) NOT NULL,
     CONSTRAINT "history_username_fkey" FOREIGN KEY (username) REFERENCES userdata(username)
 );
 
 
 INSERT INTO userdata VALUES('admin', 'admin');
 
-INSERT INTO history VALUES('admin', '2022-05-11', '25.00', '7.14', '2985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '15.00', '4.74', '5985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '45.00', '5.54', '7185.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '855.00', '8.14', '15985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '27.00', '5.75', '5185.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '254.00', '5.88', '1985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '252.00', '5.20', '2985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '210.00', '5.41', '1985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '20.00', '5.30', '985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '25.00', '5.52', '985.20');
-INSERT INTO history VALUES('admin', '2022-05-11', '25.00', '5.14', '985.20');
+INSERT INTO history VALUES('admin', '2022-05-11', '25.00', '7.14', '2985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '15.00', '4.74', '5985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '45.00', '5.54', '7185.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '855.00', '8.14', '15985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '27.00', '5.75', '5185.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '254.00', '5.88', '1985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '252.00', '5.20', '2985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '210.00', '5.41', '1985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '20.00', '5.30', '985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '25.00', '5.52', '985.20', '5400 main st. , Houston, TX, 77001');
+INSERT INTO history VALUES('admin', '2022-05-11', '25.00', '5.14', '985.20', '5400 main st. , Houston, TX, 77001');
 
 
 
-INSERT INTO userProfile VALUES('admin', 'mr.bean', '5400 University of Houston', null, 'Houston', 'TX', '77001')
+INSERT INTO userProfile VALUES('admin', 'mr.bean', '5400 main st.', null, 'Houston', 'TX', '77001')
